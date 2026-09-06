2a0e9e76b492933022eff0407df8cd9bc1c15f89
Adithya Rao Chitala <167595541+Adithyalegend24@users.noreply.github.com>
2026-09-06 11:19:44 +0530

    Add files via upload

---
+++ Week7/Movies/12.sql
@@ -0,0 +1,7 @@
+-- 12. Titles of all of movies in which both Jennifer Lawrence and Bradley Cooper starred
+SELECT m.title FROM movies m
+JOIN stars s ON m.id=s.movie_id
+JOIN people p ON s.person_id=p.id
+WHERE p.name IN ('Bradley Cooper','Jennifer Lawrence')
+GROUP BY m.title
+HAVING COUNT(DISTINCT p.name)=2;
